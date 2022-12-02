f87e44f8c4cb67c4af9007a863db746c61869988
DrAcula27 <danielle@kendallspace.net>
2022-12-01 19:58:12 -0500

    complete section 2. loops

--- javaScriptTotalRecall.js
+++ javaScriptTotalRecall.js
@@ -77,10 +77,10 @@ console.log(48 == '48');
 let animal = "dog";
 // 2. Write code that will print out "mooooo" if the it is equal to cow.
 if (animal == "cow") {
-    console.log("mooooo");
+    console.log("mooooo\n");
 } else {
     // 3. Change your code so that if the variable animal is anything other than a cow, it will print "Hey! You're not a cow."
-    console.log("Hey! You're not a cow.");
+    console.log("Hey! You're not a cow.\n");
 }
 // 4. Commit.
 
@@ -89,7 +89,100 @@ if (animal == "cow") {
 let personAge = 16;
 // Write code that will print out "Here are the keys!", if the age is 16 years or older, or, if the age is younger than 16, a message should print "Sorry, you're too young."
 if (personAge >= 16) {
-    console.log("Here are the keys!");
+    console.log("Here are the keys!\n");
 } else {
-    console.log("Sorry, you're too young.");
+    console.log("Sorry, you're too young.\n");
 }
+
+/* II. Loops
+Remember: USE letwhen you initialize your for loops!
+This is GOOD: for(let i = 0; i < 100; i++)
+This is NO GOOD: for(i = 0; i < 100; i++) */
+
+// A. The basics
+// Write a loop that will print out all the numbers from 0 to 10, inclusive.
+for(let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+// Write a loop that will print out all the numbers from 10 up to and including 400.
+for(let i = 10; i <= 400; i++) {
+    console.log(`${i}\n`);
+}
+// Write a loop that will print out every third number starting with 12 and going no higher than 4000.
+for(let i = 12; i <= 100; i += 3) {
+    console.log(`${i}\n`);
+}
+
+// B. Get even
+// Print out the numbers that are within the range of 1 - 100.
+for(let i = 1; i <= 100; i++) {
+    console.log(`${i}\n`);
+}
+// Adjust your code to add a message next to even numbers only that says: "<-- is an even number".
+for(let i = 1; i <= 100; i++) {
+    if (i % 2 == 0) {
+        console.log(`${i} <-- is an even number\n`);
+    } else {
+        console.log(`${i}\n`);
+    }
+}
+
+// C. Give me Five
+// For the numbers 0 - 100, print out "I found a number. High five!" if the number is a multiple of five
+// Example Output:
+    // I found a 5. High five!
+    // I found a 10. High five!
+for(let i = 0; i <= 100; i++) {
+    if (i % 5 == 0) {
+        console.log(`I found a ${i}. High five!\n`);
+    }
+    // or, using ternary syntax:
+    // let highFive = i % 5 == 0;
+    // console.log(highFive ? `I found a ${i}. High five!` : i);
+}
+// Add to the code from above to print out "I found a number. Three is a crowd" if the number is a multiple of three
+// Example Output:
+    // I found a 3. Three is a crowd
+    // I found a 5. High five!
+    // I found a 6. Three is a crowd
+    // I found a 9. Three is a crowd
+    // I found a 10. High five!
+    for(let i = 0; i <= 100; i++) {
+        if (i % 3 == 0) {
+            console.log(`I found a ${i}. Three is a crowd.\n`);
+        } else if (i % 5 == 0) {
+            console.log(`I found a ${i}. High five!\n`);
+        }
+        // or, using ternary syntax:
+        // let crowd = i % 3 == 0, highFive = i % 5 == 0;
+        // console.log(highFive ? `I found a ${i}. High five!` : crowd ? `I found a ${i}. Three is a crowd` : i);
+    }
+// For numbers divisible by both three and five, be sure your code prints both messages.
+for(let i = 0; i <= 100; i++) {
+    if (i % 15 == 0) {
+        console.log(`I found a ${i}. High five! Three is a crowd.\n`);
+    }else if (i % 3 == 0) {
+        console.log(`I found a ${i}. Three is a crowd.\n`);
+    } else if (i % 5 == 0) {
+        console.log(`I found a ${i}. High five!\n`);
+    }
+    // or, using ternary syntax:
+    // let crowd = i % 3 == 0, highFive = i % 5 == 0;
+    // console.log(crowd ? highFive ? `I found a ${i}. High five! Three is a crowd.` : `I found a ${i}. Three is a crowd` : highFive ? `I found a ${i}. High five!` : i);
+}
+
+// D. Savings account
+// Write code that will save the sum of all the numbers between 1 - 10 to a variable called bank_account.
+let bank_account = 0;
+for(let i = 1; i <= 10; i++) {
+    bank_account += i;
+}
+// Check your work! Your bank_account should have $55 in it.
+console.log(`Your bank account has $${bank_account} in it.\n`);
+// You got a bonus! Your pay is now doubled each week. Write code that will save the sum of all the numbers between 1 - 100 multiplied by 2.
+bank_account = 0;
+for(let i = 0; i <= 100; i++) {
+    bank_account += 2 * i;
+}
+// Check your work! Your bank_account should have $10,100 in it.
+console.log(`Your bank account has $${bank_account} in it.\n`);
