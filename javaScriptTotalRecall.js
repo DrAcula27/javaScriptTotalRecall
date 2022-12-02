69c267c0b4c37ac8a9441771fef19f40d2ebac1e
DrAcula27 <danielle@kendallspace.net>
2022-12-01 20:41:51 -0500

    complete section 3. arrays and control flow

--- javaScriptTotalRecall.js
+++ javaScriptTotalRecall.js
@@ -186,3 +186,140 @@ for(let i = 0; i <= 100; i++) {
 }
 // Check your work! Your bank_account should have $10,100 in it.
 console.log(`Your bank account has $${bank_account} in it.\n`);
+
+// III. Arrays & Control flow
+
+// A. Talk about it:
+// What are the things in an array called?
+    // -elements or items
+// Do Arrays guarantee those things will be in order?
+    // -yes, because they are given indexes
+// What real-life thing could you model with an array?
+    // -any list of items; for example, an array could hold a list of cars.
+
+// B. Easy Does It
+// Create an array that contains three quotes and store it in a variable called quotes
+let quotes = ["The future ain't what it used to be -Yogi Berra", "I have no special talent. I am only passionately curious -Albert Enstein", "All we have to decide is what to do with the time that is given us. -J.R.R. Tolkien"];
+
+// C. Accessing elements
+// Given the following array:
+const randomThings = [1, 10, "Hello", true];
+// How do you access the 1st element in the array?
+randomThings[0];
+// Change the value of "Hello"to "World"
+randomThings[2] = "World";
+// Check the value of the array to make sure it updated the array. How? Why, yes! console.log();
+console.log(randomThings);
+console.log("");
+
+// D. Change values
+// Given the following array:
+const ourClass = ["Salty", "Zoom", "Sardine", "Slack", "Github"];
+// What would you write to access the 3rd element of the array?
+ourClass[2];
+// Change the value of "Github" to "Octocat"
+ourClass[4] = "Octocat";
+// Add a new element, "Cloud City" to the array
+ourClass.push("Cloud City");
+console.log(ourClass);
+console.log("");
+
+// E. Mix It Up
+// Note: You don't really need .splice()for these. You could use it, but there are simpler array methods that are more appropriate.
+// Given the following array:
+const myArray = [5, 10, 500, 20];
+// Add the string "Aegon"to the end of the array. Add another string of your choice to the end of the array.
+myArray.push("Aegon", "Aragorn");
+// Remove the 5 from the beginning of the array.
+myArray.shift();
+// Add the string "Bob Marley" to the beginning of the array.
+myArray.unshift("Bob Marley");
+// Remove the string of your choice from the end of the array.
+myArray.pop();
+// Reverse this array using Array.prototype.reverse().
+myArray.reverse();
+// Did you mutate the array?
+    // Yes. The reverse() method transposes the elements of the calling array object in place, mutating the array, and returning a reference to the array. -source: MDN
+// What does mutate mean?
+    // A mutable value is one that can be changed without creating an entirely new value. In JavaScript, objects and arrays are mutable by default, but primitive values are not — once a primitive value is created, it cannot be changed, although the variable that holds it may be reassigned. -source: MDN
+// Did the .reverse()method return anything?
+    // Yes. The reverse() method returns the reference to the original array, now reversed. Note that the array is reversed in place, and no copy is made. -source: MDN
+console.log(myArray);
+console.log("");
+
+// F. Biggie Smalls
+// Create a variable that contains an integer.
+let int = 27;
+// Write an if ... else statement that:
+if(int < 100) {
+    // console.log()s "little number" if the number is entered is less than 100.
+    console.log("little number\n");
+} else {
+    // console.log()s big number if the number is greater than or equal to 100.
+    console.log("big number\n");
+}
+
+// G. Monkey in the Middle
+// Write an if ... else if ... else statement:
+if (int < 5) {
+    // console.log() little number if the number entered is less than 5.
+    console.log("little number\n");
+} else if (int > 10) {
+    // If the number entered is more than 10, log big number.
+    console.log("big number\n");
+} else {
+    // Otherwise, log "monkey".
+    console.log("monkey\n");
+}
+
+// H. What's in Your Closet?
+// Below, we've given you examples of Kristyn and Thom's closets modeled as data in JavaScript.
+const kristynsCloset = [
+  "left shoe",
+  "cowboy boots",
+  "right sock",
+  "GA hoodie",
+  "green pants",
+  "yellow knit hat",
+  "marshmallow peeps"
+];
+// Thom's closet is more complicated. Check out this nested data structure!!
+const thomsCloset = [
+  [
+    // These are Thom's shirts
+    "grey button-up",
+    "dark grey button-up",
+    "light blue button-up",
+    "blue button-up",
+  ],[
+    // These are Thom's pants
+    "grey jeans",
+    "jeans",
+    "PJs"
+  ],[
+    // Thom's accessories
+    "wool mittens",
+    "wool scarf",
+    "raybans"
+  ]
+];
+// What's Kristyn wearing today? Using bracket notation to access items in kristynsCloset, log the sentence "Kristyn is rocking that " + the third item in Kristyn's closet + " today!" to the console.
+console.log("Kristyn is rocking that " + kristynsCloset[2] + " today!");
+// Kristyn just bought some sweet shades! Add "raybans"to her closet after "yellow knit hat".
+kristynsCloset.splice(6, 0, "raybans");
+// Kristyn spilled coffee on her hat... modify this item to read "stained knit hat"instead of yellow.
+kristynsCloset[5] = "stained knit hat";
+console.log(kristynsCloset);
+console.log("");
+// Put together an outfit for Thom! Using bracket notation, access the first element in Thom's shirtsarray.
+let thomsShirt = thomsCloset[0][0];
+// In the same way, access one item from Thom's pants array.
+let thomsPants = thomsCloset[1][1];
+// Access one item from Thom's accessories array.
+let thomsAccessory = thomsCloset[2][1];
+// Log a sentence about what Thom's wearing. Example: "Thom is looking fierce in a grey button-up, jeans and wool scarf!"
+console.log(`Thom is looking fierce in his ${thomsShirt}, ${thomsPants} and ${thomsAccessory}!\n`);
+// Get more specific about what kind of PJs Thom's wearing this winter. Modify the name of his PJ pants to Footie Pajamas.
+thomsCloset[1][2] = "Footie Pajamas";
+console.log(thomsCloset);
+console.log("");
